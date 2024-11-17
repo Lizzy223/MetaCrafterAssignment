8cdf8f41f1983a1a761e901238327321ac5378c7
Elizabeth Kolade <koladeelizabeth01@gmail.com>
2024-11-17 13:30:26 +0000

    fix: assignment on error

--- _assignment.sol
+++ _assignment.sol
@@ -3,32 +3,19 @@
 pragma solidity ^0.8.13;
 
 contract AssignmentOnError {
-    function testRequire(uint _i) public pure {
-    
-        require(_i > 9, "Input must be greater than 9");
+    uint public amount;
+    mapping(address => uint256) public balanceOf;
+
+    // Function to deposit tokens into the contract (for testing)
+    function deposit() public payable {
+        balanceOf[msg.sender] += msg.value;
     }
 
-    function testRevert(uint _i) public pure {
-        // revert returns the gas fee used
-        if (_i <= 9) {
-            revert("Input must be greater than 9");
-        }
-    }
+    function transfer(uint256 amount) public {
+        // Check if sender has enough balance
+        require(balanceOf[msg.sender] >= amount, "Insufficient funds");
 
-    uint public num;
-
-    function testAssert() public view {
-        // since it is impossible to update the value of num
-        assert(num == 0);
-    }
-
-    // custom error
-    error InsufficientBalance(uint balance, uint withdrawAmount);
-
-    function testCustomError(uint _withdrawAmount) public view {
-        uint bal = address(this).balance;
-        if (bal < _withdrawAmount) {
-            revert InsufficientBalance({balance: bal, withdrawAmount: _withdrawAmount});
-        }
+        // Perform the transfer
+        balanceOf[msg.sender] -= amount;
     }
 }
