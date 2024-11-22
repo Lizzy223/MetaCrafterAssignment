5e3ed188a392e6879be9f3fdac0176bb80ce3c92
Elizabeth Kolade <54336149+Lizzy223@users.noreply.github.com>
2024-11-22 09:49:32 +0100

    Update openZepplinErc20token.sol

--- ERC20/openZepplinErc20token.sol
+++ ERC20/openZepplinErc20token.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    // Constructor to set the token name and symbol
+    constructor() ERC20("MyToken", "MTK") {}
+
+    // Mint function restricted to only the owner
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
